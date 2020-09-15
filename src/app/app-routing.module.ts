767ab3f5b8f693b2b011652ad3ebcf72cb446250
mngass2s <maurice.ngassa@smail.inf.h-brs.de>
2020-09-15 14:18:18 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'post-create', component: PostCreateComponent },
+  { path: '',
+    redirectTo: '/post-create',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
